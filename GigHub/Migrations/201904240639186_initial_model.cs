9adbb700812a8a4f9dd08541a3eb558892d5e520
hmuzzal <hmuzzal@mail.com>
2019-04-25 10:53:33 +0600

    InitialCommit

---
+++ GigHub/Migrations/201904240639186_initial_model.cs
@@ -0,0 +1,16 @@
+namespace GigHub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial_model : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
